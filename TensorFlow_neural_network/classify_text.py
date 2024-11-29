ff78099e14da2f706964df9dc9cbbd45841a1cd4
Dias <diasshaymerdenov@gmail.com>
2024-11-29 16:11:39 +0500

    ML study

--- TensorFlow_neural_network/classify_text.py
+++ TensorFlow_neural_network/classify_text.py
@@ -8,23 +8,32 @@ import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras import losses
 
-
-
-url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
-
-dataset = tf.keras.utils.get_file("aclImdb_v1", url, untar=True, cache_dir='.', cache_subdir='')
-
-dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 
-os.listdir(dataset_dir)
+dataset = tf.keras.utils.get_file("aclImdb_v1", "/home/dias/PycharmProjects/study/TensorFlow_neural_network/aclImdb_v1", untar=True, cache_dir='.', cache_subdir='')
+
+dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb_v1/aclImdb')
+# print(os.listdir(dataset_dir))
+
+train_dir = os.path.join(dataset_dir, 'train')
+# print(os.listdir(train_dir))
 
 
-# train_dir = os.path.join(dataset_dir, 'train')
-# os.listdir(train_dir)
-#
-#
 # sample_file = os.path.join(train_dir, 'pos/1181_9.txt')
 # with open(sample_file) as f:
 #   print(f.read())
+
+remove_dir = os.path.join(train_dir, 'unsup')
+shutil.rmtree(remove_dir)
+
+# batch_size = 32
+# seed = 42
+#
+# raw_train_ds = tf.keras.utils.text_dataset_from_directory(
+#     'aclImdb/train',
+#     batch_size=batch_size,
+#     validation_split=0.2,
+#     subset='training',
+#     seed=seed)
