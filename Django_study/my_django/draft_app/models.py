b549627e8e8a7440f3dfaa4fddeab1229b722245
dias-home-fedora <shaymerdenov.dias@bk.ru>
2025-04-28 21:29:36 +0500

    Script Editing

--- Django_study/my_django/draft_app/models.py
+++ Django_study/my_django/draft_app/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 
+class Advertisement(models.Model):
+ title = models.CharField(max_length=100)
+ price = models.PositiveIntegerField()
+ published_at = models.DateTimeField(auto_now_add=True)
+
+class Category(models.Model):
+ name = models.CharField(max_length=100)
+
+class Item(models.Model):
+ category = models.ForeignKey(Category, on_delete=models.CASCADE)
+ name = models.CharField(max_length=100)
+ price = models.DecimalField(max_digits=8, decimal_places=2)
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
