b690d77e1196f420a1ebb3a848a220dea3d251fa
Dias <diasshaymerdenov@gmail.com>
2025-07-02 15:55:45 +0500

    Script Editing

--- Django_study/my_django/draft_app/views.py
+++ Django_study/my_django/draft_app/views.py
@@ -1,5 +1,7 @@
 from django.views import View
 from django.shortcuts import render
+import requests
+from django.core.paginator import Paginator
 
 class HomePageView(View):
     def get(self, request):
@@ -7,4 +9,20 @@ class HomePageView(View):
 
 class AboutPageView(View):
     def get(self, request):
-        return render(request, 'title.html')
+        # Получаем посты
+        response = requests.get('https://jsonplaceholder.typicode.com/posts')
+        posts = response.json()
+
+        # Ручное деление
+        chunk_size = 5
+        chunk1 = posts[0:5]
+        chunk2 = posts[5:10]
+        chunk3 = posts[10:15]
+
+        context = {
+            'chunk1': chunk1,
+            'chunk2': chunk2,
+            'chunk3': chunk3,
+        }
+
+        return render(request, 'title.html', context)
